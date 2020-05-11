e21b71fe62dcd7907b56e238d6f9666197ac811e
Liam Tengelis <ltengelis@gmail.com>
2020-05-11 13:25:04 +0700

    use pip module

--- test_worker/py/main.py
+++ test_worker/py/main.py
@@ -2,20 +2,24 @@
 
 import asyncio
 import logging
-# from threading import Thread
-import time
-from typing import NamedTuple
 import os
-import sys
+from typing import NamedTuple
 
-from app import App
-from app import Frames
-import msg
+import ez_arch_worker.api as ez_worker
+
+# from app import App
+# from app import Frames
+# import msg
+
+Frames = ez_worker.Frames
 
 
 class _ENV(NamedTuple):
     PORT: int = int(os.environ["PORT"])
 ENV = _ENV()
+
+class State(NamedTuple):
+    res_count: int = 0
 
 
 def setup_logging()-> None:
@@ -29,56 +33,49 @@ def setup_logging()-> None:
 
 
 async def mock_handler(
-        app: App,
+        state: State,
         req_body: Frames
 )-> Frames:
+    new_state = state._replace(res_count = state.res_count + 1)
+    res = [b"ECHO"] + req_body
+    logging.info("new state: %s", new_state)
     await asyncio.sleep(2)
-    return [b"ECHO"] + req_body
+    return (new_state, res)
 
 
-# async def run_hb_loop(app: App)-> None:
+# async def loop_body(app: App)-> App:
+#     msg.send_heartbeat(app)
+#     items = await app.poller.poll(app.poll_interval_ms)
+#     for socket, _event in items:
+#         frames = await socket.recv_multipart()
+#         logging.info("recvd frames: %s", frames)
+#         assert b"" == frames[0]
+#         client_return_addr = frames[1]
+#         req_body = frames[2:]
+#         reply = await mock_handler(app, req_body)
+#         msg.send_response(app, client_return_addr, reply)
+#     return app
+
+
+# async def run_loop(app: App)-> None:
 #     app = await msg.connect(app)
 #     while True:
-#         msg.send_heartbeat(app)
-#         await asyncio.sleep(app.hb_interval_s)
+#         app = await loop_body(app)
 #     return
 
-
-async def loop_body(app: App)-> App:
-    msg.send_heartbeat(app)
-    items = await app.poller.poll(app.poll_interval_ms)
-    for socket, _event in items:
-        frames = await socket.recv_multipart()
-        logging.info("recvd frames: %s", frames)
-        assert b"" == frames[0]
-        client_return_addr = frames[1]
-        req_body = frames[2:]
-        reply = await mock_handler(app, req_body)
-        msg.send_response(app, client_return_addr, reply)
-    return app
-
-
-async def run_loop(app: App)-> None:
-    app = await msg.connect(app)
-    while True:
-        app = await loop_body(app)
-    return
+async def run_loop():
+    await ez_worker.run_worker(
+        service_name = b"TEST_SERVICE",
+        handler = mock_handler,
+        initial_state = State(),
+        listen_host = "localhost",
+        listen_port = ENV.PORT,
+    )
 
 
 def main():
     setup_logging()
-    app = App(
-        con_s = "tcp://127.0.0.1:{}".format(ENV.PORT),
-        service_name = b"TEST_SERVICE",
-    )
-    # t = Thread(target = run_hb_loop, args = (app,))
-    # t.start()
-    asyncio.run(run_loop(app))
-    # try:
-    #     run_work_loop(app)
-    # except Exception as e:
-    #     logging.exception("worker died: %s", e)
-    #     os._exit(1)
+    asyncio.run(run_loop())
     return
 
 
