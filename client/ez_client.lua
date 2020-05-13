61e7fe2567a61692495a53ec0bdf1263306b58f2
Liam Tengelis <ltengelis@gmail.com>
2020-05-13 12:30:16 +0700

    clean up lua client

--- client/ez_client.lua
+++ client/ez_client.lua
@@ -1,22 +1,21 @@
 local zmq = require "lzmq"
+
+
 local CLIENT = "\x02"
 local DEFAULT_TIMEOUT_MS = 3000
 local DEFAULT_ATTEMPTS = 3
+
 
 local function single_req(ctx, host, port, frames, timeout)
    if not timeout then
       timeout = DEFAULT_TIMEOUT_MS
    end
    local con_s = string.format("tcp://%s:%s", host, port)
-   log.info("connecting to %s", con_s)
    local client, err = ctx:socket{
       zmq.REQ,
       connect = con_s,
       rcvtimeo = timeout
    }
-   if err then
-      log.err("BAD %s", err)
-   end
    local request = {CLIENT}
    for _, v in pairs(frames) do
       table.insert(request, v)
